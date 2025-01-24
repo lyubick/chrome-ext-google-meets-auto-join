b5da1bb59960181e7efca8d954252435764a4fb1
lyubick <andrejs.lubimovs@gmail.com>
2025-01-24 11:25:34 +0200

    NEW.
    CW and Data display.

--- js/popup.js
+++ js/popup.js
@@ -123,3 +123,14 @@ const syncRangeText = () => {
 }
 
 syncRangeText()
+
+const getCurrentDate = () => {
+    d = new Date();
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+    const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    const weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+
+    document.getElementById('today-date').innerText =  `${new Date().toLocaleDateString().toLocaleString()} CW${weekNo}`
+}
+
+getCurrentDate()
